00831a1e11ce9aeb23467e33dc8fe2c753e07c48
MaryAnzh <maryang@mail.ru>
2022-11-01 17:09:40 +0300

    feat: add form style

--- src/view/components/form/form.component.ts
+++ src/view/components/form/form.component.ts
@@ -6,7 +6,11 @@ class FormComponent implements PageRenderer {
         const view = `
         <form class="number-form">
           <labe class="number-form__title"l>Введите число</label>
-        
+          <dov class="number-form__input-block">
+            <button class="number-form__input-block__prev">-</button>
+            <input type="number" class="number-form__input-block__field"/>
+            <button class="number-form__input-block__next">+</button>
+          </div>
         </form>
         `;
         return Promise.resolve(view);
